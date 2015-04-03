92ed39c9a377beeab33a0189f09d43eaebf1e45b
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2015-04-03 11:04:29 -0300

    First Commit
    Created indexer class

---
+++ indexer/src/indexer/ByteMaker.java
@@ -0,0 +1,28 @@
+package indexer;
+
+public class ByteMaker {
+
+	public String byteConverter(char a){
+		
+		String byte_array= a;
+		
+		
+		
+		
+		
+		return byte_array;
+	}
+	
+	
+	
+	
+	
+	
+	
+public static void main(String[] args) {
+	
+	
+	
+}	
+	
+}
