479b8c447196b9b6be7c43bb874a8b2a4701b942
Paulo Roberto Xavier <contato.pauloxavier@gmail.com>
2015-04-03 18:07:41 -0300

    Secunda Etapa
    Criado Classe de restauração de Bits, conversor pra long,
    boolean,string, inserido restore para char e para Strings

--- indexer/src/indexer/ByteMaker.java
+++ indexer/src/indexer/ByteMaker.java
@@ -1,50 +1,22 @@
 package indexer;
+import java.lang.Math;
 
 public class ByteMaker {
 
-	public static int numberOfBits=8;
-	
-	public int charConverter(char a){
-		
-		int code = (int) a;
-	
-		return code;
-	}
-	
-	public String integerConverter(int a){
-		
-		String converted = Integer.toBinaryString(a);
-		
-		String properLenghtString = sanitize(converted,4);
-		
-		return properLenghtString;
-	}
-	
-	public String binaryCharConverter(char a){
-		
-		int numToConvert = charConverter('a');
-		
-		String converted = Integer.toBinaryString(numToConvert);
-		
-		String properLenghtString = sanitize(converted,1);
-		
-		return properLenghtString;
-		
-	}
-	
+	public static int valueOfBits=8;
 	public int sizeInBits(int a){
 		
 		int size=0;
 		
 		switch(a){
 			case 2:
-				size=2*numberOfBits;
+				size=2*valueOfBits;
 				break;
 			case 4:
-				size=4*numberOfBits;
+				size=4*valueOfBits;
 				break;
 			default:
-				size=1*numberOfBits;
+				size=1*valueOfBits;
 				break;
 			}
 		
@@ -68,16 +40,70 @@ public class ByteMaker {
 		return finalString;
 	}
 	
+	//Char Converter
 	
+	public String binaryCharConverter(char a){
+		
+		int numToConvert = (int) a;
+		
+		String converted = Integer.toBinaryString(numToConvert);
+		
+		String properLenghtString = sanitize(converted,1);
+		
+		return properLenghtString;
+		
+	}
 	
-public static void main(String[] args) {
+	//int and long Converter
 	
-	ByteMaker test = new ByteMaker();
+	public String integerConverter(int a){
+		
+		String properLenghtString=new String();
+		int maxMin = (int) Math.pow(2,15);
+		
+		System.out.println(-maxMin);
+		
+		if(a< -maxMin||a> maxMin-1){
+			String converted = Integer.toBinaryString(a);
+			properLenghtString = sanitize(converted,4);
+		}
+		
+		else{	
+		
+		String converted = Integer.toBinaryString(a);
+		
+		properLenghtString = sanitize(converted,2);
+		
+		}
+		
+		return properLenghtString;
+	}
 	
-	System.out.println(test.binaryCharConverter('x'));
-	System.out.println(test.integerConverter(8));
+	//String Converter
+	
+	public String stringConverter(String a){
+		
+		char [] array = a.toCharArray(); 
+		String properLenghtString = new String();
 
+		for(int i =0;i<a.length();i++)
+		{
+		    properLenghtString+=binaryCharConverter(array[i]);
+		}
+		
+		return properLenghtString;
+	}
 	
-}	
+	//Boolean Converter
+	
+	public String booleanConverter(boolean a){
+		
+		String properLenghtString = sanitize((a?"1":"0"),1);		
+		
+		
+		return properLenghtString;
+	}
+
+
 	
 }
